acd971c4761e76da403b67df93faf9ccda1e0a99
sanvean74 <dlambertpdx@gmail.com>
2019-09-25 15:32:56 -0700

    lab working

--- server.js
+++ server.js
@@ -1,56 +1,10 @@
 require('dotenv').config();
-require('./lib/connect')();
+require('./lib/connect')(process.env.MONGODB_URI);
+const app = require('./lib/app');
+const { createServer } = require('http');
+const server = createServer(app);
 
-const express = require('express');
-const app = express();
-const Album = require('./lib/models/album');
-
-
-app.use(express.json());
-
-
-app.get('/api/albums', (req, res, next) => {
-  Album.find()
-    .then(albums => {
-      res.json(albums);
-    }) 
-    .catch(next);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
 });
-
-app.get('/api/albums/:id', (req, res, next) => {
-  Album.findById(req.params.id)
-    .then(album => {
-      res.json(album);
-    })
-    .catch(next);
-});
-
-app.post('/api/albums', (req, res, next) => {
-  Album.create(req.body)
-    .then(album => {
-      res.json(album);
-    })
-    .catch(next);
-});
-
-app.put('/api/albums/:id', (req, res, next) => {
-  Album.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true }
-  )
-    .then(album => {
-      res.json(album);
-    })
-    .catch(next);
-});
-
-app.delete('/api/albums/:id', (req, res, next) => {
-  Album.findByIdAndRemove(req.params.id)
-    .then(removed => {
-      res.json(removed);
-    })
-    .catch(next);
-});
-
-app.listen(3000, () => console.log('server is running on port 3000'));
